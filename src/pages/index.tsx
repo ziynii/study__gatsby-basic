474326ef2c8faadbf3fd252b401015414fa158a4
Jiyeon <ziyeon.ivi@gmail.com>
2022-12-18 03:44:56 +0900

    Static Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import { HeadFC, Link, PageProps } from 'gatsby';
 import Layout from '../components/Layout';
 import Seo from '../components/Seo';
+import { StaticImage } from 'gatsby-plugin-image';
 
 export default function IndexPage() {
   return (
     <Layout title="Welcome to DevStickers 👋🏻">
-      <div></div>
+      <StaticImage
+        height={500}
+        src="https://images.unsplash.com/photo-1540651810471-5699df74834f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80"
+        alt="Stickers on the wall"
+      />
     </Layout>
   );
 }
