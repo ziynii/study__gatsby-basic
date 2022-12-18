904e42c46e127115f14fbf57735779b12489d253
Jiyeon <ziyeon.ivi@gmail.com>
2022-12-18 21:36:53 +0900

    style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,20 @@ import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
   return (
     <Layout title="Welcome to DevStickers 👋🏻">
-      {data.allContentfulStickerPack.nodes.map((sticker, i) => (
-        <article key={i}>
-          <GatsbyImage
-            image={getImage(sticker.preview?.gatsbyImageData!)!}
-            alt={sticker.name!}
-          />
-          <Link to={`/products/${sticker.id}`}>
-            <h2>{sticker.name}</h2>
-            <h4>${sticker.price}</h4>
-          </Link>
-        </article>
-      ))}
+      <div className="grid">
+				{data.allContentfulStickerPack.nodes.map((sticker, i) => (
+					<article key={i}>
+						<GatsbyImage
+							image={getImage(sticker.preview?.gatsbyImageData!)!}
+							alt={sticker.name!}
+						/>
+						<Link to={`/products/${sticker.id}`}>
+							<h2>{sticker.name}</h2>
+							<h4>${sticker.price}</h4>
+						</Link>
+					</article>
+				))}
+			</div>
     </Layout>
   );
 }
