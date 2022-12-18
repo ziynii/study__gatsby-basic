dd049780cbe3ae75b7c8e4cd4a74b145ab528c26
Jiyeon <ziyeon.ivi@gmail.com>
2022-12-18 19:50:44 +0900

    Product Detail

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,8 +13,10 @@ export default function IndexPage({ data }: PageProps<Queries.StickersQuery>) {
             image={getImage(sticker.preview?.gatsbyImageData!)!}
             alt={sticker.name!}
           />
-          <h2>{sticker.name}</h2>
-          <h4>${sticker.price}</h4>
+          <Link to={`/products/${sticker.id}`}>
+            <h2>{sticker.name}</h2>
+            <h4>${sticker.price}</h4>
+          </Link>
         </article>
       ))}
     </Layout>
@@ -25,6 +27,7 @@ export const query = graphql`
   query Stickers {
     allContentfulStickerPack {
       nodes {
+				id
         price
         name
         preview {
--- src/pages/products/{contentfulStickerPack.id}.tsx
+++ src/pages/products/{contentfulStickerPack.id}.tsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import { graphql, PageProps } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import Layout from '../../components/Layout';
 
-export default function ProductDetail() {
-  return <div></div>;
+export default function ProductDetail({
+  data,
+}: PageProps<Queries.ProductQuery>) {
+  console.log(data);
+  const image = getImage(data.contentfulStickerPack?.preview?.gatsbyImageData!);
+  return (
+    <Layout title={data.contentfulStickerPack?.name!}>
+      <GatsbyImage image={image!} alt={data.contentfulStickerPack?.name!} />
+      <h2>${data.contentfulStickerPack?.price}</h2>
+    </Layout>
+  );
 }
+
+export const query = graphql`
+  query Product($id: String) {
+    contentfulStickerPack(id: { eq: $id }) {
+      name
+      price
+      preview {
+        gatsbyImageData(height: 450, placeholder: BLURRED)
+      }
+    }
+  }
+`;
